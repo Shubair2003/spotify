0d2f18acbae4409f4c678f6a7f5cd32e7f47bb5d
Shubair2003 <155351402+Shubair2003@users.noreply.github.com>
2025-02-18 12:37:57 +0530

    Update PlayerContext.jsx

--- spotify-clone/src/context/PlayerContext.jsx
+++ spotify-clone/src/context/PlayerContext.jsx
@@ -9,7 +9,7 @@ const PlayerContextProvider = (props) => {
     const seekBg = useRef();
     const seekBar = useRef();
 
-    const url = process.env.SPOTIFY_BACKEND_URL || 'http://localhost:4000'
+    const url = import.meta.env.SPOTIFY_BACKEND_URL || 'http://localhost:4000'
 
     const [songsData,setSongsData] = useState([]);
     const [albumsData,setAlbumsData] = useState([]);
