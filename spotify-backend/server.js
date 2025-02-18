81780ba6fd800d5bc03cb9b5e37e1f6c38cef861
Shubair <shubair2003@gmail.com>
2025-02-18 11:49:21 +0530

    first commit

---
+++ spotify-backend/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import songRouter from './src/routes/songRoute.js';
+import connectDB from './src/config/mongoDB.js';
+import connectCloudinary from './src/config/cloudinary.js';
+import albumRouter from './src/routes/albumRoute.js';
+
+//app config
+const app = express();
+const port = process.env.PORT || 4000;
+
+connectDB();
+connectCloudinary();
+
+//middleswares
+app.use(express.json());
+app.use(cors());
+
+//initializing routes
+app.use("/api/song", songRouter)
+app.use("/api/album", albumRouter)
+
+app.get('/', (rew,res)=> res.send("API Working"))
+
+app.listen(port, ()=>console.log(`Server startd on ${port}`))
