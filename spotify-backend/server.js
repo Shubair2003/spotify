18d360f6a8e3a9d5d32982541dfb695790041234
Shubair2003 <155351402+Shubair2003@users.noreply.github.com>
2025-02-18 12:16:57 +0530

    Update server.js

--- spotify-backend/server.js
+++ spotify-backend/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import songRouter from './src/routes/songRoute.js';
-import connectDB from './src/config/mongoDB.js';
+import connectDB from './src/config/mongodb.js';
 import connectCloudinary from './src/config/cloudinary.js';
 import albumRouter from './src/routes/albumRoute.js';
 
